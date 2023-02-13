4c218c2292b3ea9c1b121e564a3bab20af850d6f
Brendan Lau <brendanlau2926@gmail.com>
2023-02-13 16:10:33 +0800

    hi

--- engine.cpp
+++ engine.cpp
@@ -67,7 +67,7 @@ class InstrumentOrderBook{
 		Node* curr = sell_head->next;
 		if (curr == nullptr) { // no matching resting order
 			// add buy order to buy resting
-			// call insertBuy(order)
+			insertBuy(&order);
 			Output::OrderAdded(order.order_id, order.instrument.c_str(), order.price, order.count, false, order.timestamp);
 			return;
 		} 
