02cd29c35cd12a214cba93df33504556d8e7f578
Yeat Nai Jie <e0550613@u.nus.edu>
2023-02-17 14:39:04 +0800

    edit

--- engine.cpp
+++ engine.cpp
@@ -14,9 +14,11 @@ void InstrumentOrderBook::tryExecuteBuy(Order& order) {
 	Node* curr = sell_head->next;
 	if (curr == nullptr) { // no matching resting order
 		// add buy order to buy resting
+		lk.unlock();
 		insertBuy(order, std::move(buy_head_lk));
 		// Output::OrderAdded(order.order_id, order.instrument.c_str(), order.price, order.count, false, getCurrentTimestamp());
 		return;
+		
 	} 
 	std::unique_lock<std::mutex> lk_2(curr->m);
 	int totalCount = order.count;
@@ -24,6 +26,7 @@ void InstrumentOrderBook::tryExecuteBuy(Order& order) {
 		Order& match = *(curr->order);
 		if (match.price > order.price) {
 			order.count = totalCount;
+			lk.unlock();
 			insertBuy(order, std::move(buy_head_lk));
 			// Output::OrderAdded(order.order_id, order.instrument.c_str(), order.price, order.count, false,
 			// 	getCurrentTimestamp());
@@ -54,6 +57,7 @@ void InstrumentOrderBook::tryExecuteBuy(Order& order) {
 		if (curr->next == nullptr) {
 			if (totalCount > 0) {
 				order.count = totalCount;
+				lk.unlock();
 				insertBuy(order, std::move(buy_head_lk));
 				// Output::OrderAdded(order.order_id, order.instrument.c_str(), order.price, order.count, false,
 				// 	getCurrentTimestamp()); 
@@ -73,6 +77,7 @@ void InstrumentOrderBook::tryExecuteSell(Order& order) {
 	Node* curr = buy_head->next;
 	if (curr == nullptr) { // no matching resting order
 		// add buy order to buy resting
+		lk.unlock();
 		insertSell(order, std::move(sell_head_lk));
 		// Output::OrderAdded(order.order_id, order.instrument.c_str(), order.price, order.count, true, getCurrentTimestamp());
 		return;
@@ -83,6 +88,7 @@ void InstrumentOrderBook::tryExecuteSell(Order& order) {
 		Order& match = *(curr->order);
 		if (match.price < order.price) {
 			order.count = totalCount;
+			lk.unlock();
 			insertSell(order, std::move(sell_head_lk));
 			// Output::OrderAdded(order.order_id, order.instrument.c_str(), order.price, order.count, true,
 			// 	getCurrentTimestamp());
@@ -114,6 +120,7 @@ void InstrumentOrderBook::tryExecuteSell(Order& order) {
 		if (curr->next == nullptr) {
 			if (totalCount > 0) {
 				order.count = totalCount;
+				lk.unlock();
 				insertSell(order, std::move(sell_head_lk)); 
 				// Output::OrderAdded(order.order_id, order.instrument.c_str(), order.price, order.count, true,
 				// 	getCurrentTimestamp());
@@ -212,11 +219,21 @@ void InstrumentOrderBook::tryCancel(int target_order_id) {
 void InstrumentOrderBook::insertBuy(Order& order, std::unique_lock<std::mutex> buy_head_lk ) {
 	
 	// dummy mutex
-	std::mutex mut;
-	Node* curr = nullptr;
-	Node* curr_next = buy_head;
-	std::unique_lock<std::mutex> curr_lk(mut);
-	std::unique_lock<std::mutex> curr_next_lk = std::move(buy_head_lk);
+	// std::mutex mut;
+	// Node* curr = nullptr;
+	// Node* curr_next = buy_head;
+	// std::unique_lock<std::mutex> curr_lk(mut);
+	// std::unique_lock<std::mutex> curr_next_lk = std::move(buy_head_lk);
+	std::unique_lock<std::mutex> curr_lk = std::move(buy_head_lk);
+	Node* curr = buy_head;
+	Node* curr_next = buy_head->next;
+	if(curr_next == nullptr) {
+		Node* node = new Node(nullptr, &order);
+		curr->next = node;
+		Output::OrderAdded(order.order_id, order.instrument.c_str(), order.price, order.count, false, getCurrentTimestamp());
+		return;
+	}
+	std::unique_lock<std::mutex> curr_next_lk(curr_next->m);
 	while(curr_next != nullptr && (curr_next->order->price >= order.price || curr_next == buy_head)) {
 		curr_lk.swap(curr_next_lk);
 		if(curr_next->next != nullptr) {
@@ -243,11 +260,20 @@ void InstrumentOrderBook::insertBuy(Order& order, std::unique_lock<std::mutex> b
 
 void InstrumentOrderBook::insertSell(Order& order, std::unique_lock<std::mutex> sell_head_lk) {
 	// dummy mutex
-	std::mutex mut;
-	Node* curr = nullptr;
-	Node* curr_next = sell_head;
-	std::unique_lock<std::mutex> curr_lk(mut);
-	std::unique_lock<std::mutex> curr_next_lk = std::move(sell_head_lk);
+	// std::mutex mut;
+	// Node* curr = nullptr;
+	// Node* curr_next = sell_head;
+	std::unique_lock<std::mutex> curr_lk = std::move(sell_head_lk);
+	Node* curr = sell_head;
+	Node* curr_next = sell_head->next;
+	if(curr_next == nullptr) {
+		Node* node = new Node(nullptr, &order);
+		curr->next = node;
+		Output::OrderAdded(order.order_id, order.instrument.c_str(), order.price, order.count, true, getCurrentTimestamp());
+		return;
+	}
+	std::unique_lock<std::mutex> curr_next_lk(curr_next->m);
+	// std::unique_lock<std::mutex> curr_next_lk = std::move(sell_head_lk);
 	while(curr_next != nullptr && (curr_next->order->price <= order.price ||  curr_next == sell_head)) {
 		curr_lk.swap(curr_next_lk);
 		if(curr_next->next != nullptr) {
